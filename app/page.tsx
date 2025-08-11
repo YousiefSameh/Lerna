64a519a4705006d3d3662b145b5ccdfc8c5ef279
Yousief Sameh <yousief.sameh@outlook.com>
2025-08-11 13:16:27 +0300

    Create Login Ui

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,11 @@
 import { Button } from "@/components/ui/button";
+import { ThemeToggle } from "@/components/ui/theme-toggle";
 
 export default function Home() {
   return (
-    <Button>Hello, World!</Button>
+    <>
+      <Button>Hello, World!</Button>
+      <ThemeToggle />
+    </>
   )
 }
