ae6b46877af63def5aa349b11987a2d6a41fc5da
Yousief Sameh <yousief.sameh@outlook.com>
2025-08-19 07:30:44 +0300

    Protect Endpoints

--- app/admin/courses/create/actions.ts
+++ app/admin/courses/create/actions.ts
@@ -1,16 +1,50 @@
 "use server";
 
-import { auth } from "@/lib/auth";
+import { requireAdmin } from "@/app/data/admin/require-admin";
+import arcjet, { detectBot, fixedWindow } from "@/lib/arcjet";
 import { prisma } from "@/lib/db";
 import { ApiResponse } from "@/lib/types";
 import { courseSchema, CourseSchemaType } from "@/lib/zodSchemas";
-import { headers } from "next/headers";
+import { request } from "@arcjet/next";
 
-export async function CreateCourse(values: CourseSchemaType): Promise<ApiResponse> {
-  const session = await auth.api.getSession({
-    headers: await headers()
-  });
+const aj = arcjet
+  .withRule(
+    detectBot({
+      mode: "LIVE",
+      allow: [],
+    })
+  )
+  .withRule(
+    fixedWindow({
+      mode: "LIVE",
+      window: "1m",
+      max: 5,
+    })
+  );
+
+export async function CreateCourse(
+  values: CourseSchemaType
+): Promise<ApiResponse> {
+  const session = await requireAdmin();
   try {
+    const req = await request();
+    const decision = await aj.protect(req, {
+      fingerprint: session.user.id,
+    });
+
+    if (decision.isDenied()) {
+      if (decision.reason.isRateLimit()) {
+        return {
+          status: "error",
+          message: "You have been blocked due to rate limiting",
+        };
+      } else {
+        return {
+          status: "error",
+          message: "You are a bot!, if this is a mistake contact our support",
+        };
+      }
+    }
     const validation = courseSchema.safeParse(values);
 
     if (!validation.success) {
@@ -28,13 +62,13 @@ export async function CreateCourse(values: CourseSchemaType): Promise<ApiRespons
     });
 
     return {
-      status: 'success',
-      message: "Course created successfully"
-    }
+      status: "success",
+      message: "Course created successfully",
+    };
   } catch {
     return {
-      status: 'error',
-      message: "Something went error"
-    }
+      status: "error",
+      message: "Something went error",
+    };
   }
 }
