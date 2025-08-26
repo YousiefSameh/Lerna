b4eb4b6a067c8f7f73ee38141b0b05336d3a314f
Yousief Sameh <yousief.sameh@outlook.com>
2025-08-26 18:55:34 +0300

    Creating and Deleting Lessons/Chapters

---
+++ app/admin/courses/[courseId]/edit/_components/NewLessonModal.tsx
@@ -0,0 +1,121 @@
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import { tryCatch } from "@/lib/try-catch";
+import { lessonSchema, LessonSchemaType } from "@/lib/zodSchemas";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { Loader2, Plus } from "lucide-react";
+import { useState, useTransition } from "react";
+import { useForm } from "react-hook-form";
+import { createLesson } from "../actions";
+import { toast } from "sonner";
+import { Button } from "@/components/ui/button";
+
+export function NewLessonModal({
+  courseId,
+  chapterId,
+}: {
+  courseId: string;
+  chapterId: string;
+}) {
+  const [isOpen, setIsOpen] = useState(false);
+  const [pending, startTransition] = useTransition();
+
+  const form = useForm<LessonSchemaType>({
+    resolver: zodResolver(lessonSchema),
+    defaultValues: {
+      name: "",
+      courseId,
+      chapterId,
+
+    },
+  });
+
+  async function onSubmit(values: LessonSchemaType) {
+    startTransition(async () => {
+      const { data: result, error } = await tryCatch(createLesson(values));
+
+      if (error) {
+        toast.error(error.message || "Something went wrong. Please try again.");
+        return;
+      }
+
+      if (result.status === "success") {
+        toast.success(result.message);
+        form.reset();
+        setIsOpen(false);
+      } else if (result.status === "error") {
+        toast.error(result.message);
+      }
+    });
+  }
+
+  function handleOpenChange(open: boolean) {
+    if (!open) {
+      form.reset();
+    }
+    setIsOpen(open);
+  }
+
+  return (
+    <Dialog open={isOpen} onOpenChange={handleOpenChange}>
+      <DialogTrigger asChild>
+        <Button variant="outline" className="w-full justify-center gap-1">
+          <Plus className="size-4" /> New Lesson
+        </Button>
+      </DialogTrigger>
+      <DialogContent className="sm:max-w-[425px]">
+        <DialogHeader>
+          <DialogTitle>Create New Lesson</DialogTitle>
+          <DialogDescription>
+            What would you like to name your lesson?
+          </DialogDescription>
+        </DialogHeader>
+        <Form {...form}>
+          <form className="space-y-8" onSubmit={form.handleSubmit(onSubmit)}>
+            <FormField
+              control={form.control}
+              name="name"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Name</FormLabel>
+                  <FormControl>
+                    <Input placeholder="Lesson Name" {...field} />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <DialogFooter>
+              <Button disabled={pending} type="submit">
+                {pending ? (
+                  <>
+                    <Loader2 className="size-4 animate-spin" />
+                    Creating...
+                  </>
+                ) : (
+                  <>Create Lesson</>
+                )}
+              </Button>
+            </DialogFooter>
+          </form>
+        </Form>
+      </DialogContent>
+    </Dialog>
+  );
+}
--- app/admin/courses/[courseId]/edit/actions.ts
+++ app/admin/courses/[courseId]/edit/actions.ts
@@ -4,7 +4,14 @@ import { requireAdmin } from "@/app/data/admin/require-admin";
 import arcjet from "@/lib/arcjet";
 import { prisma } from "@/lib/db";
 import { ApiResponse } from "@/lib/types";
-import { courseSchema, CourseSchemaType } from "@/lib/zodSchemas";
+import {
+  chapterSchema,
+  ChapterSchemaType,
+  courseSchema,
+  CourseSchemaType,
+  lessonSchema,
+  LessonSchemaType,
+} from "@/lib/zodSchemas";
 import { detectBot, fixedWindow, request } from "@arcjet/next";
 import { revalidatePath } from "next/cache";
 
@@ -109,18 +116,18 @@ export async function reorderLessons(
     return {
       status: "success",
       message: "Lessons reordered successfully",
-    }
+    };
   } catch {
     return {
       status: "error",
       message: "An unexpected error occured. Please try again.",
-    }
+    };
   }
 }
 
 export async function reorderChapters(
   courseId: string,
-  chapters: { id: string; position: number }[],
+  chapters: { id: string; position: number }[]
 ): Promise<ApiResponse> {
   await requireAdmin();
   try {
@@ -150,6 +157,242 @@ export async function reorderChapters(
     return {
       status: "success",
       message: "Chapters reordered successfully",
+    };
+  } catch {
+    return {
+      status: "error",
+      message: "An unexpected error occured. Please try again.",
+    };
+  }
+}
+
+export async function createChapter(
+  values: ChapterSchemaType
+): Promise<ApiResponse> {
+  await requireAdmin();
+  try {
+    const result = chapterSchema.safeParse(values);
+    if (!result.success) {
+      return {
+        status: "error",
+        message: "Invalid data",
+      };
+    }
+    await prisma.$transaction(async (tx) => {
+      const maxPos = await tx.chapter.findFirst({
+        where: { courseId: result.data.courseId },
+        select: { position: true },
+        orderBy: { position: "desc" },
+      });
+
+      await tx.chapter.create({
+        data: {
+          title: result.data.name,
+          courseId: result.data.courseId,
+          position: (maxPos?.position ?? 0) + 1,
+        },
+      });
+    });
+
+    revalidatePath(`/admin/courses/${result.data.courseId}/edit`);
+    return {
+      status: "success",
+      message: "Chapter created successfully",
+    };
+  } catch {
+    return {
+      status: "error",
+      message: "An unexpected error occured. Please try again.",
+    };
+  }
+}
+
+export async function createLesson(
+  values: LessonSchemaType
+): Promise<ApiResponse> {
+  await requireAdmin();
+  try {
+    const result = lessonSchema.safeParse(values);
+    if (!result.success) {
+      return {
+        status: "error",
+        message: "Invalid data",
+      };
+    }
+    await prisma.$transaction(async (tx) => {
+      const maxPos = await tx.lesson.findFirst({
+        where: { chapterId: result.data.chapterId },
+        select: { position: true },
+        orderBy: { position: "desc" },
+      });
+
+      await tx.lesson.create({
+        data: {
+          title: result.data.name,
+          description: result.data.description,
+          videoKey: result.data.videoKey,
+          thumbnailKey: result.data.thumbnailKey,
+          chapterId: result.data.chapterId,
+          position: (maxPos?.position ?? 0) + 1,
+        },
+      });
+    });
+
+    revalidatePath(`/admin/courses/${result.data.courseId}/edit`);
+    return {
+      status: "success",
+      message: "Lesson created successfully",
+    };
+  } catch {
+    return {
+      status: "error",
+      message: "An unexpected error occured. Please try again.",
+    };
+  }
+}
+
+export async function deleteLesson({
+  lessonId,
+  chapterId,
+  courseId,
+}: {
+  lessonId: string;
+  chapterId: string;
+  courseId: string;
+}): Promise<ApiResponse> {
+  await requireAdmin();
+  try {
+    const chapterWithLesssons = await prisma.chapter.findFirst({
+      where: {
+        id: chapterId,
+        courseId,
+      },
+      select: {
+        lessons: {
+          orderBy: {
+            position: "asc",
+          },
+          select: {
+            id: true,
+            position: true,
+          },
+        },
+      },
+    });
+
+    if (!chapterWithLesssons) {
+      return {
+        status: "error",
+        message: "Chapter not found",
+      };
+    }
+
+    const lessons = chapterWithLesssons.lessons;
+    const lessonToDelete = lessons.find((lesson) => lesson.id === lessonId);
+
+    if (!lessonToDelete) {
+      return {
+        status: "error",
+        message: "Lesson not found",
+      };
+    }
+
+    const remainingLessons = lessons.filter(
+      (lesson) => lesson.id !== lessonToDelete.id
+    );
+
+    const updates = remainingLessons.map((lesson, index) =>
+      prisma.lesson.update({
+        where: { id: lesson.id },
+        data: { position: index + 1 },
+      })
+    );
+
+    await prisma.$transaction([
+      ...updates,
+      prisma.lesson.delete({
+        where: { id: lessonToDelete.id, chapterId },
+      }),
+    ]);
+
+    revalidatePath(`/admin/courses/${courseId}/edit`);
+    return {
+      status: "success",
+      message: "Lesson deleted successfully",
+    };
+  } catch {
+    return {
+      status: "error",
+      message: "An unexpected error occured. Please try again.",
+    };
+  }
+}
+
+export async function deleteChapter({
+  chapterId,
+  courseId,
+}: {
+  chapterId: string;
+  courseId: string;
+}): Promise<ApiResponse> {
+  await requireAdmin();
+  try {
+    const courseWithChapters = await prisma.course.findFirst({
+      where: {
+        id: courseId,
+      },
+      select: {
+        chapters: {
+          orderBy: {
+            position: "asc",
+          },
+          select: {
+            id: true,
+            position: true,
+          },
+        },
+      },
+    });
+
+    if (!courseWithChapters) {
+      return {
+        status: "error",
+        message: "Course not found",
+      };
+    }
+
+    const chapters = courseWithChapters.chapters;
+    const chapterToDelete = chapters.find((chapter) => chapter.id === chapterId);
+
+    if (!chapterToDelete) {
+      return {
+        status: "error",
+        message: "Chapter not found",
+      };
+    }
+
+    const remainingChapters = chapters.filter(
+      (chapter) => chapter.id !== chapterToDelete.id
+    );
+
+    const updates = remainingChapters.map((chapter, index) =>
+      prisma.chapter.update({
+        where: { id: chapter.id },
+        data: { position: index + 1 },
+      })
+    );
+
+    await prisma.$transaction([
+      ...updates,
+      prisma.chapter.delete({
+        where: { id: chapterId },
+      }),
+    ]);
+
+    revalidatePath(`/admin/courses/${courseId}/edit`);
+    return {
+      status: "success",
+      message: "Chapter deleted successfully",
     };
   } catch {
     return {
