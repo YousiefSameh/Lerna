62bc46655b1af9594716cb6ef8b7faed9e4cf6cb
Yousief Sameh <yousief.sameh@outlook.com>
2025-11-18 04:53:40 +0200

    Creating Analytics Page

---
+++ app/data/admin/admin-get-dashboard-stats.ts
@@ -0,0 +1,31 @@
+"use server";
+
+import { prisma } from "@/lib/db";
+import { requireAdmin } from "./require-admin";
+
+export async function getAdminDashboardStats() {
+  await requireAdmin();
+  const [totalSignups, totalCustomers, totalCourses, totalLessons] = await Promise.all([
+    // Total signups
+    prisma.user.count(),
+    // Total customers
+    prisma.user.count({
+      where: {
+        enrollment: {
+          some: {},
+        },
+      },
+    }),
+    // Total courses
+    prisma.course.count(),
+    // Total lessons
+    prisma.lesson.count(),
+  ]);
+
+  return {
+    totalSignups,
+    totalCustomers,
+    totalCourses,
+    totalLessons,
+  }
+}
