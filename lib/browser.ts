ad389864cb02bc61fad9832d3de41bee3f635069
Yousief Sameh <yousief.sameh@outlook.com>
2025-11-22 06:16:19 +0200

    fix the chromium error

--- lib/browser.ts
+++ lib/browser.ts
@@ -2,11 +2,20 @@ import chromium from "@sparticuz/chromium";
 import { chromium as playwrightChromium } from "playwright-core";
 
 export async function getBrowser() {
-  const executablePath = await chromium.executablePath();
-  const browser = await playwrightChromium.launch({
-    args: chromium.args,
-    executablePath,
+  const isProduction = process.env.NODE_ENV === "production";
+
+  if (isProduction) {
+    chromium.setGraphicsMode = false;
+    const executablePath = await chromium.executablePath();
+
+    return await playwrightChromium.launch({
+      args: chromium.args,
+      executablePath,
+      headless: true,
+    });
+  }
+
+  return await playwrightChromium.launch({
     headless: true,
   });
-  return browser;
 }
